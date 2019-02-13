8cf327bf2d5f09e97db0b1fcee84f01a9391d915
Neel Bhavsar <neel.bhavsar@ryerson.ca>
2019-02-12 23:35:04 -0500

    added files

---
+++ Hotlines/index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="keywords" content="">
+		<meta name="author" content="">
+		<link rel="icon" href="">
+
+		<title>
+
+			Suicide Prevention
+
+		</title>
+
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    	<link rel="stylesheet" href="animate.min.css">
+			<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<link rel="stylesheet" href="../dist/aos.css" />
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.4/js/tether.min.js"></script>
+		<script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+
+		<style type="text/css">
+
+		body,h1 {font-family: "Raleway", Arial, sans-serif}
+		h1 {letter-spacing: 6px}
+		.w3-row-padding img {margin-bottom: 8px}
+		
+		#center{
+			margin:auto;
+		}
+		
+		#jumbo{
+			background-image: url(prevent.jpg);
+			background-position: 0% 25%;
+			background-size: cover;
+			background-repeat: no-repeat;
+		}
+
+		</style>
+
+	</head>
+
+	<body class="bg-dark">
+	
+		<nav id="topbar" class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
+		
+			<a class="navbar-brand text-danger" href="../">Suicide Prevention</a>
+			<button class="navbar-toggler border-danger" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="nav nav-tabs nav-light mr-auto" role="tablist">
+					<li class="nav-item">
+						<a class="nav-link" style="color:#E2704E;"href="../">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" style="color:#E2704E;"href="../chat.php">Chat</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" style="color:#E2704E;" href="">Hotlines</a>
+					</li>
+				</ul>
+			</div>
+			
+		</nav>
+
+		<div class="container" style="margin-top:50px;">
+			<div class="jumbotron bg-dark text-center">
+				<h1 class="display-4 text-danger">Renascent Munro Treatment Centre</h1>
+				<p class="lead text-danger">Address: 356 Dundas St W, Toronto, ON M5T 1G4<br>Hours: Open 24 hours<br>Province: Ontario<br>Phone: (416) 847-6441</p>
+				<h1 class="display-4 text-danger"></h1>
+				<p class="lead text-danger">Non-profit organization in Toronto, Ontario<br>Address: 10 Trinity Square, Toronto, ON M5G 1B1<br>Hours: Opens 10 a.m. Mon.</p>
+			</div>
+		</div>
+		
+		<script type="text/javascript">
+
+
+
+		</script>
+
+	</body>
+
+</html>
---
+++ chat.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="keywords" content="">
+		<meta name="author" content="">
+		<link rel="icon" href="">
+
+		<title>
+
+			Suicide Prevention
+
+		</title>
+
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    	<link rel="stylesheet" href="animate.min.css">
+			<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<link rel="stylesheet" href="../dist/aos.css" />
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.4/js/tether.min.js"></script>
+		<script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+
+		<style type="text/css">
+
+		body{
+			margin:0;
+			padding:0;
+		}
+
+		</style>
+
+	</head>
+
+	<body class="bg-dark">
+	
+		<nav id="topbar" class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
+		
+			<a class="navbar-brand text-danger" href="index.php">Suicide Prevention</a>
+			<button class="navbar-toggler border-danger" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="nav nav-tabs nav-light mr-auto" role="tablist">
+					<li class="nav-item">
+						<a class="nav-link" style="color:#E2704E;"href="index.php">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" style="color:#E2704E;"href="">Chat</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" style="color:#E2704E;" href="Hotlines/">Hotlines</a>
+					</li>
+				</ul>
+			</div>
+			
+		</nav>			
+			
+		<div class="container-fluid center text-center mt-5">
+			<iframe width="100%" height="850px" allow="microphone;" src="https://console.dialogflow.com/api-client/demo/embedded/b0528b96-5edb-4dbc-9bb1-de18465546be"></iframe>
+		</div>
+		
+		<script type="text/javascript">
+
+
+
+		</script>
+
+	</body>
+
+</html>
